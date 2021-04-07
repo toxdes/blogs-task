77162341b027d3e9d56d53eee4efcfef17e59944
toxicdesire <malivp3494@gmail.com>
2021-04-08 01:10:27 +0530

    Fixing deployment issues

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosConfig = {
-  baseURL: "http://jsonplaceholder.typicode.com",
+  baseURL: "https://jsonplaceholder.typicode.com",
   headers: {
     "Content-Type": "application/json; charset=UTF-8",
   },
